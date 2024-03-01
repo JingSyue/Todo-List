f3e1579e06b1f63543295c94d16f88f67ec4703c
JingSyue <acs108801@gm.ntcu.edu.tw>
2024-03-01 21:19:23 +0800

    pass the test of basic CRUD login register functions

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,25 +4,36 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs'); // 確保引入bcrypt
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const auth = require('../middlewares/authMiddleware');
 
 // 注册
 router.post('/register', async (req, res) => {
   try {
-    // 檢查數據庫中是否存在相同的電子郵件地址
+    // 检查数据库中是否存在相同的电子邮件地址
     const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
-      return res.status(400).send({ error: '用戶已存在' });
+      return res.status(400).send({ error: '用户已存在' });
     }
 
-    // 如果用戶不存在，則創建新用戶
+    // 如果用户不存在，则创建新用户
     const user = new User(req.body);
-    await user.save();
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    
+    // 将token添加到用户的tokens数组中
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
 
-    res.status(201).send({ user, token });
+    res.status(201).send({ 
+      user: {
+        id: user._id,
+        email: user.email
+        // 其他需要发送的用户信息
+      }, 
+      token 
+    });
   } catch (error) {
     console.error(error);
-    res.status(400).send({ error: '註冊過程中發生錯誤', details: error.message});
+    res.status(400).send({ error: '注册过程中发生错误', details: error.message});
   }
 });
 
@@ -38,9 +49,36 @@ router.post('/login', async (req, res) => {
       return res.status(401).send({ error: 'Invalid login credentials' });
     }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-    res.send({ user, token });
+
+    // 将新token添加到用户的tokens数组中
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    res.status(201).send({ 
+      user: {
+        id: user._id,
+        email: user.email
+        // 其他需要发送的用户信息
+      }, 
+      token 
+    });
   } catch (error) {
     res.status(400).send({ error: 'An error occurred during login' });
+  }
+});
+
+
+// 登出
+router.post('/logout', auth, async (req, res) => {
+  try {
+    // 从用户的tokens数组中移除当前的token
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send({ message: 'Logged out successfully' });
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred during logout' });
   }
 });
 
